640c0c79df0c7540f19cc957aeb84dfd204dcff3
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-23 23:08:13 -0600

    Drawing the Ship to the Screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 # import settings       #------1 below
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -22,6 +23,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))  #-----Surface
         
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)                            #------self is AlienInvasion, the game
         
         
         # # Set the background color.
@@ -39,6 +42,8 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
