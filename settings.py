9a5a58cbb729b0fa16775063b982355d2bb94843
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-27 21:39:01 -0600

    Adding the Bullet Settings

--- settings.py
+++ settings.py
@@ -13,6 +13,11 @@ class Settings:
         self.ship_speed = 1.5
         # self.bg_color = (132, 143, 113)
 
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
 
 
 
