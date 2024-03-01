bde4516e2924c726529c2c62347a86188676d36a
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-29 21:59:27 -0600

    Making Sure to Score All Hits

--- alien_invasion.py
+++ alien_invasion.py
@@ -233,7 +233,9 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
 
