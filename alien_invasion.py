3753ce9f41b38e2fa27c87909cd2bb741a592db6
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-29 21:56:12 -0600

    Resetting the Score

--- alien_invasion.py
+++ alien_invasion.py
@@ -172,6 +172,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -234,7 +235,7 @@ class AlienInvasion:
         if collisions:
             self.stats.score += self.settings.alien_points
             self.sb.prep_score()
-            
+
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet
