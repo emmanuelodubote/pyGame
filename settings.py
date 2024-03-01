6869a0cbb8ab5c66b02af9b3835f8133383ba3b4
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-29 22:42:49 -0600

    final commit

--- settings.py
+++ settings.py
@@ -15,13 +15,13 @@ class Settings:
         # self.bg_color = (132, 143, 113)
 
         # self.bullet_speed = 1.5
-        self.bullet_width = 30
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings.
-        self.alien_speed = 1.0
+        self.alien_speed = 0.5
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up/
@@ -41,7 +41,7 @@ class Settings:
 
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 0.8
+        self.alien_speed = 0.5
 
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
