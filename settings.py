744225cfd289e2d0ede33dcce2c189b4ceb43fd1
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-29 20:02:43 -0600

    Speeding Up the Bullets

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_speed = 1.5
         # self.bg_color = (132, 143, 113)
 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
