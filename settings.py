c4758d714947fee8a439c09a0f1caf10c412c66e
Emmanuel <emmanuel_odubote1@baylor.edu>
2024-02-29 22:38:21 -0600

    Displaying the Number of Ships

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ class Settings:
 
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = 0.8
 
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
--- ship.py
+++ ship.py
@@ -1,15 +1,17 @@
 
 
 import pygame
+from pygame.sprite import Sprite
 
 
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
 
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
@@ -63,7 +65,7 @@ class Ship:
 
     def center_ship(self):
         """Center the ship on the screen."""
-        
+
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
 
